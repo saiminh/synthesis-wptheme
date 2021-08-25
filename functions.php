38b99690c126dc94ac5f8a2fc2872472c42d8236
saiminh <saiminh@gmail.com>
2021-08-25 08:42:45 +0200

    is it the style.css blurb at the top?

--- functions.php
+++ functions.php
@@ -243,38 +243,38 @@ add_shortcode( 'earlier-investments', 'outputbuffer_earlier_investments' );
  * Get Dependency Plugins for theme
  */
 
-// require_once get_template_directory() . '/class-tgm-plugin-activation.php';
-// add_action( 'tgmpa_register', 'synthesiscapital_register_required_plugins' );
+require_once get_template_directory() . '/class-tgm-plugin-activation.php';
+add_action( 'tgmpa_register', 'synthesiscapital_register_required_plugins' );
 
-// function synthesiscapital_register_required_plugins() {
-//   /*
-//    * Array of plugin arrays. Required keys are name and slug.
-//    * If the source is NOT from the .org repo, then source is also required.
-//    */
-//   $plugins = array(
-//     array(
-//       'name'      => 'SVG Support',
-//       'slug'      => 'svg-support',
-//       'required'  => false,
-//     ),
-//     array(
-//       'name'      => 'Duplicate Page',
-//       'slug'      => 'duplicate-page',
-//       'required'  => false,
-//     )
-//   );
-//     $config = array(
-//       'id'           => 'synthesiscapital',      // Unique ID for hashing notices for multiple instances of TGMPA.
-//       'default_path' => '',                      // Default absolute path to bundled plugins.
-//       'menu'         => 'tgmpa-install-plugins', // Menu slug.
-//       'parent_slug'  => 'themes.php',            // Parent menu slug.
-//       'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
-//       'has_notices'  => true,                    // Show admin notices or not.
-//       'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
-//       'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
-//       'is_automatic' => false,                   // Automatically activate plugins after installation or not.
-//       'message'      => '',    
-//     );
+function synthesiscapital_register_required_plugins() {
+  /*
+   * Array of plugin arrays. Required keys are name and slug.
+   * If the source is NOT from the .org repo, then source is also required.
+   */
+  $plugins = array(
+    array(
+      'name'      => 'SVG Support',
+      'slug'      => 'svg-support',
+      'required'  => false,
+    ),
+    array(
+      'name'      => 'Duplicate Page',
+      'slug'      => 'duplicate-page',
+      'required'  => false,
+    )
+  );
+    $config = array(
+      'id'           => 'synthesiscapital',      // Unique ID for hashing notices for multiple instances of TGMPA.
+      'default_path' => '',                      // Default absolute path to bundled plugins.
+      'menu'         => 'tgmpa-install-plugins', // Menu slug.
+      'parent_slug'  => 'themes.php',            // Parent menu slug.
+      'capability'   => 'edit_theme_options',    // Capability needed to view plugin install page, should be a capability associated with the parent menu used.
+      'has_notices'  => true,                    // Show admin notices or not.
+      'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+      'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+      'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+      'message'      => '',    
+    );
 
-//     tgmpa( $plugins, $config );
-//   }
+    tgmpa( $plugins, $config );
+  }
