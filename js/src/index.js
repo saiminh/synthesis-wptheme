f41bac4fe06fc008418a48b89d62f99a16c4b1a3
saiminh <saiminh@gmail.com>
2021-08-31 16:33:55 +0200

    vh help for mobile

--- js/src/index.js
+++ js/src/index.js
@@ -3,6 +3,11 @@ import { bgGradients } from './components/bg-gradients';
 import { focusAnimation } from './components/focus-animation';
 import { cookieConsent } from './components/cookie-consent';
 
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
 bgGradients();
 cookieConsent();
 
