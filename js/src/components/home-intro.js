8ca2bbd8281d6fedbf8be4d2bf2c3f5af38c7133
saiminh <saiminh@gmail.com>
2021-09-21 14:19:56 +0200

    home page animation

--- js/src/components/home-intro.js
+++ js/src/components/home-intro.js
@@ -10,24 +10,48 @@ function homeIntro() {
   }
   invertBgGrad();
 
+  let bgGrad = document.createElement('div');
+  bgGrad.classList.add('hero-bg-gradient');
+  document.querySelector('.hero-home').append(bgGrad);
+
   let tl = gsap.timeline({
     onComplete: () => { invertBgGrad(); }
   });
     tl.to('.home ul.menu li', {
       autoAlpha: 1,
-      stagger: .2
-    }, 2)
-    .to('.site-header .logo', {
-      scale: 1,
-      duration: 1,
-      transformOrigin: "100% 0%",
-      ease: "circ.inOut"
-    }, 2)
-    .to( '.hero-home h2', {
+      stagger: {
+        amount: .5,
+        duration: 1
+      },
+      duration: 3,
+      ease: 'power3.out'
+    }, 3)
+    .fromTo( 'lottie-player', {
+      autoAlpha: 0
+    }, {
       autoAlpha: 1,
-      duration: 2,
-      ease: 'circ.out'
-    }, 2);
+      stagger: {
+        amount: .5,
+        duration: .5
+      },
+      duration: 2.5
+    }, 0.25)
+    .fromTo('.home .hero-home p', {
+      autoAlpha: 0
+    }, {
+      autoAlpha: 1,
+      duration: 3,
+      ease: 'power3.out'
+    }, 3)
+    .fromTo('.home .hero-home .hero-bg-gradient', {
+      transformOrigin: "50% 100%",
+      scaleY: 0
+    }, {
+      scaleY: 1,
+      ease: 'power2.inOut',
+      duration: 3
+    }, .25)
+    ;
 
   gsap.to('#logo-fill', {
     yPercent: -66.666,
