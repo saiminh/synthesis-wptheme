74f4c72350a2df91714f992e7455069f6037b6ec
saiminh <saiminh@gmail.com>
2021-09-22 21:46:56 +0200

    touchend instead?

--- js/src/components/side-scroll-navigation.js
+++ js/src/components/side-scroll-navigation.js
@@ -68,52 +68,9 @@ function sideScrollNavigation(){
       })
       
       // on touch-swipe, re-enable scroll snap
-      scroller.addEventListener('touchstart', handleTouchStart, false);        
-      scroller.addEventListener('touchmove', handleTouchMove, false);
-
-      var xDown = null;                                                        
-      var yDown = null;
-
-      function getTouches(evt) {
-        return evt.touches ||             // browser API
-               evt.originalEvent.touches; // jQuery
-      }                                                     
-                                                                         
-      function handleTouchStart(evt) {
-          const firstTouch = getTouches(evt)[0];                                      
-          xDown = firstTouch.clientX;                                      
-          yDown = firstTouch.clientY;                                      
-      };                                                
-                                                                         
-      function handleTouchMove(evt) {
-          if ( ! xDown || ! yDown ) {
-              return;
-          }
-
-          var xUp = evt.touches[0].clientX;                                    
-          var yUp = evt.touches[0].clientY;
-
-          var xDiff = xDown - xUp;
-          var yDiff = yDown - yUp;
-                                                                         
-          if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-            scroller.style.scrollSnapType = 'x mandatory';
-              if ( xDiff > 0 ) {
-                  /* right swipe */ 
-              } else {
-                  /* left swipe */
-              }                       
-          } else {
-              if ( yDiff > 0 ) {
-                  /* down swipe */ 
-              } else { 
-                  /* up swipe */
-              }                                                                 
-          }
-          /* reset values */
-          xDown = null;
-          yDown = null;                                             
-      };
+      scroller.addEventListener('touchend', () => {
+        scroller.style.scrollSnapType = 'x mandatory';
+      })
 
 
       if ( scroller.scrollWidth > window.innerWidth ) {
