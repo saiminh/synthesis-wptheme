8534b3731b34b3dfc075ae858a8871fdc01f8939
saiminh <saiminh@gmail.com>
2021-09-22 21:24:50 +0200

    different approach to touchevents

--- js/src/components/side-scroll-navigation.js
+++ js/src/components/side-scroll-navigation.js
@@ -62,12 +62,60 @@ function sideScrollNavigation(){
         }      
       }
 
+      // When mousewheel or touchpad is used, re-enable scroll snap
       scroller.addEventListener('wheel', () => {
         scroller.style.scrollSnapType = 'x mandatory';
       })
-      scroller.addEventListener('touchmove', () => {
-        scroller.style.scrollSnapType = 'x mandatory';
-      })
+      
+      // on touch-swipe, re-enable scroll snap
+      scroller.addEventListener('touchstart', handleTouchStart, false);        
+      scroller.addEventListener('touchmove', handleTouchMove, false);
+
+      var xDown = null;                                                        
+      var yDown = null;
+
+      function getTouches(evt) {
+        return evt.touches ||             // browser API
+               evt.originalEvent.touches; // jQuery
+      }                                                     
+                                                                         
+      function handleTouchStart(evt) {
+          const firstTouch = getTouches(evt)[0];                                      
+          xDown = firstTouch.clientX;                                      
+          yDown = firstTouch.clientY;                                      
+      };                                                
+                                                                         
+      function handleTouchMove(evt) {
+          if ( ! xDown || ! yDown ) {
+              return;
+          }
+
+          var xUp = evt.touches[0].clientX;                                    
+          var yUp = evt.touches[0].clientY;
+
+          var xDiff = xDown - xUp;
+          var yDiff = yDown - yUp;
+                                                                         
+          if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+              if ( xDiff > 0 ) {
+                  /* right swipe */ 
+                  scroller.style.scrollSnapType = 'x mandatory';
+              } else {
+                  /* left swipe */
+                  scroller.style.scrollSnapType = 'x mandatory';
+              }                       
+          } else {
+              if ( yDiff > 0 ) {
+                  /* down swipe */ 
+              } else { 
+                  /* up swipe */
+              }                                                                 
+          }
+          /* reset values */
+          xDown = null;
+          yDown = null;                                             
+      };
+
 
       if ( scroller.scrollWidth > window.innerWidth ) {
         scroller.parentElement.classList.add('showNavBtns');
