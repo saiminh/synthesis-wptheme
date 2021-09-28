b42f761f407c2e9580719274da0477f37a22f9cf
saiminh <saiminh@gmail.com>
2021-09-28 13:54:48 +0200

    implement GA w cookie control

--- js/src/components/cookie-consent.js
+++ js/src/components/cookie-consent.js
@@ -1,78 +1,103 @@
 import CC from 'cookieconsent';
 
 function cookieConsent(){
-  // var num = 1;
-  //  function addCookie(){
-  //     document.cookie = num+" = "+num;
-  //     num++;
-  //  }
-  //  function listCookies(){
-  //     var result = document.cookie;
-  //     document.getElementById("list").innerHTML = result;
-  //  }
-  //  function removeCookies() {
-  //     var res = document.cookie;
-  //     var multiple = res.split(";");
-  //     for(var i = 0; i < multiple.length; i++) {
-  //        var key = multiple[i].split("=");
-  //        document.cookie = key[0]+" =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
-  //     }
-  //  }
-
-  //  document.getElementById('addcookie').addEventListener('click',  () => addCookie() );
-  //  document.getElementById('listcookies').addEventListener('click', () => listCookies() );
-  //  document.getElementById('removecookies').addEventListener('click', () => removeCookies() );
 
   function clearCookies() {
+    
     var cookies = document.cookie.split("; ");
     for (var c = 0; c < cookies.length; c++) {
-        var d = window.location.hostname.split(".");
-        while (d.length > 0) {
-            var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
-            var p = location.pathname.split('/');
-            document.cookie = cookieBase + '/';
-            while (p.length > 0) {
-                document.cookie = cookieBase + p.join('/');
-                p.pop();
-            };
-            d.shift();
-        }
+      var d = window.location.hostname.split(".");
+      while (d.length > 0) {
+        var cookieBase = encodeURIComponent(cookies[c].split(";")[0].split("=")[0]) + '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; domain=' + d.join('.') + ' ;path=';
+        var p = location.pathname.split('/');
+        document.cookie = cookieBase + '/';
+        while (p.length > 0) {
+          document.cookie = cookieBase + p.join('/');
+          p.pop();
+        };
+        d.shift();
+      }
     }
   };
 
+  function addCookies() {
+
+    //creating asynchronous loadScript function
+    const loadScript = (src, async = true, type = "text/javascript") => {
+      return new Promise((resolve, reject) => {
+        try {
+          const el = document.createElement("script");
+          const container = document.head || document.body;
+
+          el.type = type;
+          el.async = async;
+          el.src = src;
+
+          el.addEventListener("load", () => {
+            resolve({ status: true });
+          });
+
+          el.addEventListener("error", () => {
+            reject({
+              status: false,
+              message: `Failed to load the script ${src}`
+            });
+          });
+
+          container.appendChild(el);
+        } 
+        catch (err) {
+          reject(err);
+        }
+      })
+    };
+
+    //asynchronously loading GA
+    loadScript("https://www.googletagmanager.com/gtag/js?id=G-BTHDQ2MN8L")
+    .then((data) => {
+        console.log("Facebook script loaded successfully", data);
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-BTHDQ2MN8L');
+    })
+    .catch((err) => {
+        console.error(err);
+    })
+
+  };
+
   window.cookieconsent.initialise({
-    type: 'opt-out',
-    revokable:true,
+    type: 'opt-in',
+    revokable: true,
     onInitialise: function (status) {
       var type = this.options.type;
       var didConsent = this.hasConsented();
       if (type == 'opt-in' && didConsent) {
-        // enable cookies
+        addCookies();
       }
       if (type == 'opt-out' && !didConsent) {
-        // disable cookies
-        // do nothing I guess
+        clearCookies();
       }
     },
     onStatusChange: function(status, chosenBefore) {
+      console.log(this.hasConsented() ? 'enable cookies' : 'disable cookies');
       var type = this.options.type;
       var didConsent = this.hasConsented();
       if (type == 'opt-in' && didConsent) {
-        // enable cookies
+        addCookies();
       }
       if (type == 'opt-out' && !didConsent) {
-        // disable cookies
         clearCookies();
       }
     },
     onRevokeChoice: function() {
       var type = this.options.type;
       if (type == 'opt-in') {
-        // disable cookies
         clearCookies();
       }
       if (type == 'opt-out') {
-        // enable cookies
+        addCookies();
       }      
     },
     onPopupClose: function() {
